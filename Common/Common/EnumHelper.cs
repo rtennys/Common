97c9f7c28d9a671ebca16faf78f7fad76c696d03
Rob Tennyson <rob@robtennyson.us>
2011-12-20 09:12:52 -0600

    EnumHelper

--- Common/Common/EnumHelper.cs
+++ Common/Common/EnumHelper.cs
@@ -9,6 +9,11 @@ namespace Common
             return Enum.GetNames(typeof(T));
         }
 
+        public static T[] GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).As<T[]>();
+        }
+
         public static T Parse<T>(string value)
         {
             return (T)Enum.Parse(typeof(T), value);
