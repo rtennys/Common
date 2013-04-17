8a527256101807450d1a89a22a9cd30574fff575
Rob Tennyson <rob@robtennyson.us>
2013-04-17 13:10:16 -0500

    Added DynamicTypeBuilder

---
+++ Common/Common.Tests/DynamicTypeBuilderTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Common.Tests
+{
+    [TestFixture]
+    public class DynamicTypeBuilderTests
+    {
+        private IDynamicTypeBuilderFactory _sut;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            _sut = new DynamicTypeBuilderFactory();
+        }
+
+        [Test]
+        public void Can_create_simple_type()
+        {
+            // Act
+            var type = _sut.CreateTypeBuilder("MyTestTypeName").AddProperty<string>("StringProperty").AddProperty<int>("IntProperty").CreateType();
+
+            // Assert
+            Assert.IsNotNull(type);
+            Assert.IsNotNull(type.GetProperty("StringProperty"));
+            Assert.IsNotNull(type.GetProperty("IntProperty"));
+            Assert.IsNull(type.GetProperty("NonExistentProperty"));
+        }
+
+        [Test]
+        public void Can_create_multiple_types()
+        {
+            // Act
+            var type1 = _sut.CreateTypeBuilder("MyTestTypeName1").AddProperty<string>("StringProperty").CreateType();
+            var type2 = _sut.CreateTypeBuilder("MyTestTypeName2").AddProperty<string>("StringProperty").CreateType();
+
+            // Assert
+            Assert.IsNotNull(type1);
+            Assert.IsNotNull(type2);
+            Assert.AreNotEqual(type1, type2);
+        }
+
+        [Test]
+        public void Can_create_instance_of_created_type()
+        {
+            // Arrange
+            var type = _sut.CreateTypeBuilder("MyTestTypeName").AddProperty<string>("StringProperty").AddProperty<int>("IntProperty").CreateType();
+
+            // Act
+            dynamic instance = Activator.CreateInstance(type);
+
+            // Assert
+            Assert.IsNotNull(instance);
+            Assert.IsNull(instance.StringProperty);
+            Assert.AreEqual(0, instance.IntProperty);
+        }
+    }
+}
---
+++ Common/Common/DynamicTypeBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Common
+{
+    public interface IDynamicTypeBuilderFactory
+    {
+        IDynamicTypeBuilder CreateTypeBuilder(string typeName);
+        IDynamicTypeBuilder CreateTypeBuilder(string typeName, string assemblyName);
+    }
+
+    public interface IDynamicTypeBuilder
+    {
+        Type CreateType();
+        IDynamicTypeBuilder AddProperty<T>(string propertyName);
+        IDynamicTypeBuilder AddProperty(Type propertyType, string propertyName);
+    }
+
+    public class DynamicTypeBuilderFactory : IDynamicTypeBuilderFactory
+    {
+        public IDynamicTypeBuilder CreateTypeBuilder(string typeName)
+        {
+            return CreateTypeBuilder(typeName, "_DynamicTypeAssembly");
+        }
+
+        public IDynamicTypeBuilder CreateTypeBuilder(string typeName, string assemblyName)
+        {
+            return new Inner(typeName, assemblyName);
+        }
+
+        private class Inner : IDynamicTypeBuilder
+        {
+            public Inner(string typeName, string assemblyName)
+            {
+                _typeBuilder = AppDomain.CurrentDomain
+                    .DefineDynamicAssembly(new AssemblyName(assemblyName), AssemblyBuilderAccess.Run)
+                    .DefineDynamicModule(assemblyName)
+                    .DefineType(typeName, TypeAttributes.Public);
+
+                _typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+            }
+
+            private readonly TypeBuilder _typeBuilder;
+
+            public Type CreateType()
+            {
+                return _typeBuilder.CreateType();
+            }
+
+            public IDynamicTypeBuilder AddProperty<T>(string propertyName)
+            {
+                return AddProperty(typeof(T), propertyName);
+            }
+
+            public IDynamicTypeBuilder AddProperty(Type propertyType, string propertyName)
+            {
+                var fieldBuilder = _typeBuilder.DefineField(string.Concat("m_", propertyName), propertyType, FieldAttributes.Private);
+                var propertyMethodAttributes = MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig;
+
+                var getterMethod = _typeBuilder.DefineMethod(string.Concat("get_", propertyName), propertyMethodAttributes, propertyType, Type.EmptyTypes);
+                var getterILCode = getterMethod.GetILGenerator();
+                getterILCode.Emit(OpCodes.Ldarg_0);
+                getterILCode.Emit(OpCodes.Ldfld, fieldBuilder);
+                getterILCode.Emit(OpCodes.Ret);
+
+                var setterMethod = _typeBuilder.DefineMethod(string.Concat("set_", propertyName), propertyMethodAttributes, null, new[] {propertyType});
+                var setterILCode = setterMethod.GetILGenerator();
+                setterILCode.Emit(OpCodes.Ldarg_0);
+                setterILCode.Emit(OpCodes.Ldarg_1);
+                setterILCode.Emit(OpCodes.Stfld, fieldBuilder);
+                setterILCode.Emit(OpCodes.Ret);
+
+                var propertyBuilder = _typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+                propertyBuilder.SetGetMethod(getterMethod);
+                propertyBuilder.SetSetMethod(setterMethod);
+
+                return this;
+            }
+        }
+    }
+}
