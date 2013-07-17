83788a13add10bf1028162ad9e827b2ae7252500
rtennys <rob@robtennyson.us>
2013-07-17 10:20:09 -0500

    ExpressionHelper.GetName - added check for UnaryExpression

---
+++ Common/Common.Tests/ExpressionHelperTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Common.Tests
+{
+    [TestFixture]
+    public class ExpressionHelperTests
+    {
+        [Test]
+        public void Can_get_memberexpression_name()
+        {
+            Assert.AreEqual("PropertyOne", ExpressionHelper.GetName<TestClass>(x => x.PropertyOne));
+        }
+
+        [Test]
+        public void Can_get_unaryexpression_name()
+        {
+            Assert.AreEqual("PropertyTwo", ExpressionHelper.GetName<TestClass>(x => x.PropertyTwo));
+        }
+
+        private class TestClass
+        {
+            public string PropertyOne { get; set; }
+            public Guid PropertyTwo { get; set; }
+        }
+    }
+}
